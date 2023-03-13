1ae3f7957cfb33d3e80a58d24041f7799f710b98
August Johansson <august.johansson@sintef.no>
2023-03-13 15:13:03 +0100

    Use larger reltol

--- Tests/Utilities/verifyStruct.m
+++ Tests/Utilities/verifyStruct.m
@@ -5,7 +5,7 @@ function verifyStruct(testCase, state, refstate)
     import matlab.unittest.constraints.RelativeTolerance
 
     % Compare each data point in state with refstate
-    reltol = 1e-6;
+    reltol = 1e-5;
     fprintf('state.Control.E %f\n', state.Control.E);
     fprintf('refstate.Control.E %f\n', refstate.Control.E);
     testCase.assertThat(state, IsEqualTo(refstate, ...
